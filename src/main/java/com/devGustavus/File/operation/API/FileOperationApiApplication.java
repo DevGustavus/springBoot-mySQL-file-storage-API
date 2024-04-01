ae041a99a3da59d57955a50ef82df11181457c87
gustavo <gustavusinvictus@gmail.com>
2024-03-31 23:05:09 -0300

    projeto base

---
+++ src/main/java/com/devGustavus/File/operation/API/FileOperationApiApplication.java
@@ -0,0 +1,13 @@
+package com.devGustavus.File.operation.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileOperationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileOperationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devGustavus/File/operation/API/FileOperationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devGustavus.File.operation.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileOperationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
